58979dabee6d0d4ece038cdfa28c1aa52877dcdf
sebyjt <sjkappan@gmail.com>
2019-02-08 15:00:50 +0530

    optimiaztions

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,14 @@ var colors=[];
     controller = new PageController();
     controller2 = new SwiperController();
   for(int i=0;i<5;i++)
-      {width.add(200.0);
+      {
       if(i==0)
-        colors.add(Colors.transparent);
+        {colors.add(Colors.transparent);
+        width.add(10.0);
+        }
       else
-      colors.add(Colors.white.withAlpha(1000));}
+      {colors.add(Colors.white.withAlpha(1000));
+      width.add(200.0);}}
   }
 
   @override
@@ -40,74 +43,91 @@ var colors=[];
         new Expanded(
             flex: 1,
             child: Container(
-                height: 50.0,
-                child: Swiper(
-                  controller: controller2,
-                  viewportFraction: .4,
-                  scale: .01,
-                  loop: false,
-                  autoplay: false,
-                  scrollDirection: Axis.horizontal,
-                  onIndexChanged: (i) {
-                    setState(() {
-                      print("hello");
-                      for(int ind=0;ind<5;ind++) {
-                        if(i==ind)
-                        {width[ind]=10.0;
-                        colors[i]=Colors.transparent;}
-                        else
-                        {width[ind] = 200.0;
-                        colors[ind]=Colors.white.withAlpha(1000);
-                        }
-                      }
-                    });
-                    controller.animateToPage(i,
-                        duration: Duration(milliseconds: 200),
-                        curve: Curves.linear);
-                  },
-                  itemBuilder: (context, i) => new GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          print("hello");
-                          for(int ind=0;ind<5;ind++) {
-                            if(i==ind)
-                              {width[ind]=10.0;
-                               colors[i]=Colors.transparent;}
-                            else
-                            {width[ind] = 200.0;
-                            colors[ind]=Colors.white.withAlpha(1000);}
+                child: Center(
+                  child: Swiper(
+                    controller: controller2,
+                    viewportFraction: .4,
+                    scale: .01,
+                    loop: false,
+                    autoplay: false,
+                    scrollDirection: Axis.horizontal,
+                    onIndexChanged: (i) {
+                      setState(() {
+                        print("hello");
+                        for(int ind=0;ind<5;ind++) {
+                          if(i==ind)
+                          {width[ind]=10.0;
+                          colors[i]=Colors.transparent;}
+                          else
+                          {width[ind] = 200.0;
+                          colors[ind]=Colors.white.withAlpha(1000);
                           }
-                        });
-                        controller2.move(i, animation: true);
-                        controller.animateToPage(i,
-                            duration: Duration(milliseconds: 10000),
-                            curve: Curves.linear);
+                        }
+                      });
+                      controller.animateToPage(i,
+                          duration: Duration(milliseconds: 200),
+                          curve: Curves.linear);
+                    },
+                    itemBuilder: (context, i) => new GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            print("hello");
+                            for(int ind=0;ind<5;ind++) {
+                              if(i==ind)
+                                {width[ind]=10.0;
+                                 colors[i]=Colors.transparent;}
+                              else
+                              {width[ind] = 200.0;
+                              colors[ind]=Colors.white.withAlpha(1000);}
+                            }
+                          });
+                          controller2.move(i, animation: true);
+                          controller.animateToPage(i,
+                              duration: Duration(milliseconds: 10000),
+                              curve: Curves.linear);
 
-                      },
-                      child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Stack(
-                            fit: StackFit.expand,
-                              children:[
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height*.08),
+                            child: Container(
 
-                            Image.network(
-                              "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201805/Mohanlal_bday.jpeg?HzRB5uJkFPQJvnYaUcYTa9WSwd3Jv9rc"),
-                            Positioned(
-                              bottom: MediaQuery.of(context).size.height*.125,
-                              left: MediaQuery.of(context).size.width*.001,
-                              child:Container(
-                                width: MediaQuery.of(context).size.width*.35,
-                                  child:new Center(child: AnimatedContainer(duration: Duration(milliseconds: 300),height: 5,
-                                margin: EdgeInsets.symmetric(horizontal: width[i] ),
-                                color: Colors.white,
-                                width: MediaQuery.of(context).size.width*.35,),
+
+
+                              child: Card(
+                                elevation: 10.0,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0 )),
+                                child: Stack(
+                                  fit: StackFit.expand,
+                                    children:[
+
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0 ),
+                                      child: Image.network(
+                                      "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201805/Mohanlal_bday.jpeg?HzRB5uJkFPQJvnYaUcYTa9WSwd3Jv9rc",fit: BoxFit.fill,),
+                                  ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                      child: Container(
+                                        margin: EdgeInsets.only(bottom: 5.0),
+                                  height: 5.0,
+                              child: new Center(
+                                child: new AnimatedContainer(
+                                  duration: Duration(milliseconds: 300),
+                                  margin: EdgeInsets.symmetric(horizontal: width[i]),
+                                  color: Colors.white,
+                                ),
+                              ),
+                                  ),
+                                ),
+                                      Container(
+                                        color: colors[i],
+                                      )
+
+                           ]),
+                              ),
                             ))),
-                                Container(
-                                  color: colors[i],
-                                )
-
-                         ]))),
-                  itemCount: 5,
+                    itemCount: 5,
+                  ),
                 ))),
         Expanded(
             flex: 2,
